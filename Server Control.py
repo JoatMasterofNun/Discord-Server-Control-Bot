d6dc18cf25e726a4b68b0115d2ce21e2440ca099
JoatMasterofNun <JoatMasterofNun@users.noreply.github.com>
2017-11-26 22:20:30 -0600

    Update Server Control.py

--- Server Control.py
+++ Server Control.py
@@ -1,15 +1,15 @@
-
+OS is Debian, Jessie
 
 #Things we need variables / global variables for
 gamelist=['ARK','7D2D','FOREST','RUST','L4D2'] #Names of games servers exist for. There are also matching roles in discord the same name as these.
 #Map names for games that will support multiple?
 ark_map=['TheIsland','TheCenter','Ragnarok','ScorchedEarth_P']
-7D2D_map=['map1','map2'] #Insert game names here for check
+7D2D_map=['map1','map2'] #Insert game names here for check.
 current_game
 approved_roles=['ADMIN','MOD'] #add the name of the roles that can stop any server
 start_user #To store user who initiated server
 server_running=0
-current_server='char'
+current_server='$game from gamelist'
 server_PID
 
 #command to start server from bot
@@ -35,7 +35,7 @@ if message(!start ARK)
 	#waits for message with map name
 	#inserts value in place of ark_map (below) in start argument
 	start ShooterGameServer.exe ark_map?SessionName=potato?ServerPassword=potato?ServerAdminPassword=****?Port=?QueryPort=?listen?MaxPlayers=1 exit
-	#capture PID and store as server_PID
+	#capture PID via popen and store as server_PID
 	#set server_running to 1
 	#Deletes chats and outputs "Ark server is running"
 
@@ -43,7 +43,7 @@ if message(!start ARK)
 if message(!stop)
 	#check if ADMIN or MOD is in message.author.roles
 	#else check if userid matches start_user
-	#killPID command sent with server_PID
+	#killPID command sent with server_PID to shell
 	#set server_running to 0
 	#Deletes chat and outputs "No server is running"
 else
