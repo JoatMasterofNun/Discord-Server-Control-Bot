386eef7b2bc917f0eaa06140b249e570b4335af5
JoatMasterofNun <JoatMasterofNun@users.noreply.github.com>
2017-11-27 06:26:41 -0600

    Update Server Control.py

--- Server Control.py
+++ Server Control.py
@@ -12,13 +12,14 @@ server_running=0
 current_server='$game from gamelist'
 server_PID
 
-#command to start server from bot
+#commands to start server from bot
 !start Rust
 #starts Rust for example
-#But bot checks user roles
+#But bot checks user roles for a match
 #Bot also checks if server_running=1, will not start a server if one is already active
 if server_running=1
 	message #- 'current_server' is currently running
+	return
 elif ('RUST' in message.author.roles) #Possibly a if statement reading in a message with the !start removed, rather than an if for every game
 	#start rust using popen to capture PID to server_PID
 	#save userid to start_user so only that user can stop server
@@ -27,10 +28,14 @@ elif ('RUST' in message.author.roles) #Possibly a if statement reading in a mess
 else
 	return
 
-#if the game has multiple maps, send the request for map name
+#if the game has multiple maps, send the request for map name after the !start command
 if message(!start ARK)
-	#Check user roles for permission
 	#Check if server_running=0
+	if server_running=1
+		message #- 'current_server' is currently running
+		return
+	elif
+	#Check user roles for match again
 	#Send new message asking for map with $map 'ark_map'
 	#waits for message with map name
 	#inserts value in place of ark_map (below) in start argument
@@ -42,7 +47,9 @@ if message(!start ARK)
 #Stop sequence
 if message(!stop)
 	#check if ADMIN or MOD is in message.author.roles
+	killpid server_PID
 	#else check if userid matches start_user
+	killpid server_PID
 	#killPID command sent with server_PID to shell
 	#set server_running to 0
 	#Deletes chat and outputs "No server is running"
